a23cd18f992c16d1685d7de33f92d45945e8ba0c
kevinLL22 <lopez.kevin.45678@gmail.com>
2023-05-26 22:47:14 -0500

    creating user and component for user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UserService} from "./service/user.service";
+import {User} from "./interface/user";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'forum-front';
+
+  private user: User = {
+    'name': 'Junior Graham',
+    'username': 'junio123',
+    'email': 'Sincere@april.biz',
+    'address': {
+      'street': 'Kulas Light',
+      'suite': 'Apt. 556',
+      'city': 'Gwenborough',
+      'zipcode': '92998-3874',
+      'geo': {
+        'lat': '-37.3159',
+        'lng': '81.1496'
+      }
+    },
+    'phone': '1-770-736-8031 x56442',
+    'website': 'hildegard.org',
+    'company': {
+      'name': 'Romaguera-Crona',
+      'catchPhrase': 'Multi-layered client-server neural-net',
+      'bs': 'harness real-time e-markets'
+    }
+  };
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.onGetUsers();
+    //this.onGetUser();
+    this.onCreateUser()
+  }
+  onGetUsers(): void {
+    this.userService.getUsers().subscribe(
+      (response) => console.table(response),
+      (error: any) => console.log(error),
+      () => console.log('Done getting users'),
+
+    );
+  }
+  onGetUser(): void {
+    this.userService.getUser().subscribe(
+      (response) => console.log(response),
+      (error: any) => console.log(error),
+      () => console.log('Done getting user'),
+
+    );
+  }
+  onCreateUser(): void {
+    this.userService.createUser(this.user).subscribe(
+      (response) => console.log(response),
+      (error: any) => console.log(error),
+      () => console.log('Done creating user'),
+
+    );
+  }
+
 }
+
---
+++ src/app/service/user.service.ts
@@ -0,0 +1,30 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {User} from "../interface/user";
+import {environment} from "src/environments/environment.staging";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {
+  }
+
+
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.apiUrl + '/users');
+  }
+
+  getUser(): Observable<User> {
+    return this.http.get<User>(this.apiUrl + '/users/1');
+  }
+
+  createUser(user: User): Observable<User> {
+    return this.http.post<User>(this.apiUrl +'/users', user);
+  }
+
+}
